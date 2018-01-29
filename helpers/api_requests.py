b0782a9e64a1606c51086de050d1e4a283cedb0d
sergeymoroz <sergeymoroz0307@gmail.com>
2018-01-29 13:23:06 +0200

    fix token presentation

--- helpers/api_requests.py
+++ helpers/api_requests.py
@@ -17,18 +17,18 @@ class ApiRequest(object):
         r = requests.post(BASE_URL + url, json.dumps(payload), headers={'Authorization': 'Bearer ' + token})
         return json.loads(r.text)
 
-    def get_user(self, startindex=None, maxresults=None, includeguests=None, includedeleted=None):
+    def get_user(self, startindex=None, maxresults=None, includeguests=None, includedeleted=None, token=None):
         return self._get('/v2/user', {'start-index': startindex,
                                       'max-results': maxresults,
                                       'include-guests': includeguests,
                                       'include-deleted': includedeleted},
-                         token='nJQhJG3Kg60vubHtug2msT9yc4O7CUPFXwI442hN')
+                         token=token)
 
     def create_user(self, name=None, roles=[], title='', mention_name=None, is_group_admin=False,
-                    timezone='UTC', password='', email=None):
+                    timezone='UTC', password='', email=None, token=None):
         return self._post('/v2/user', {'name': name, 'roles': roles, 'title': title, 'mention': mention_name,
                                        'is_group_admin': is_group_admin, 'timezoze': timezone, 'password': password,
-                                       'email': email}, token='PZDYCP8IjMT13L5YdZEguOK4mxtlOCH1m9snfvYv')
+                                       'email': email}, token=token)
 
-    def view_user(self, id_or_email=None):
-        return self._get('/v2/user/'+id_or_email, None, token='nJQhJG3Kg60vubHtug2msT9yc4O7CUPFXwI442hN')
+    def view_user(self, id_or_email=None, token=None):
+        return self._get('/v2/user/'+id_or_email, None, token=token)
