b579b025a2791ed120f5890d3febf823f35cfa84
DESKTOP-HRI2PAR\Евгений <e.anedchenko@room8studio.com>
2018-01-30 23:07:14 +0200

    Done, with hardcode

--- features/steps/step_checking-user-name-on-people-page.py
+++ features/steps/step_checking-user-name-on-people-page.py
@@ -1,6 +1,19 @@
-from behave import when
+from behave import when, then
+
+
+# from features.steps.step_checking_status import FULL_NAME
 
 
 @when("we compare name of current user with name on Welcome title")
 def step_impl(context):
-    assert 'Aned4enko_test' in context.authorized_page.get_page_head()
+    assert "Aned4enko" in context.authorized_page.get_page_head()
+
+
+@when("we move to user page (on People Page)")
+def step_impl(context):
+    context.people_page.open_user_page()
+
+
+@then("we compare name of current user with name in user page")
+def step_impl(context):
+    assert "Aned4enko" in context.people_page.label_page_head_on_user_page()
--- pages/people_page.py
+++ pages/people_page.py
@@ -1,15 +1,13 @@
 from .base_page import Page
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
-from random import randint
+import time
+# from random import randint
 
 
 class PeoplePage(Page):
-
     url = '/people'
     admins = ['Tim Bortnik']
-
-
 
     def show_admins_only(self):
         self.context.driver.find_element_by_id('show_admins_only').click()
@@ -24,3 +22,11 @@ class PeoplePage(Page):
 
     def we_see(self):
         self.context.wait.until(EC.visibility_of_element_located((By.XPATH, '//ol[@class="aui-nav-pagination"]')))
+
+    def open_user_page(self):
+        self.context.driver.find_element_by_xpath(
+            '//*[@id="content"]/div/div/section/table/tbody/tr[1]/td[1]/span[2]/a').click()
+        time.sleep(5)
+
+    def label_page_head_on_user_page(self):
+        return self.context.driver.find_element_by_css_selector("div.aui-item > h2").text
