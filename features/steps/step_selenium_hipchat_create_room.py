11d54d9ab3db020f5564790033e8f869e58768c6
genasmetchik@gmail.com <genasmetchik@gmail.com>
2018-02-18 23:34:30 +0200

    add assert on accept invite

--- features/steps/step_selenium_hipchat_create_room.py
+++ features/steps/step_selenium_hipchat_create_room.py
@@ -29,7 +29,7 @@ def step_impl(context):
 
 @then('we accept the invitation')
 def step_impl(context):
-    context.lobby_page.accept_invite()
+    assert context.lobby_page.accept_invite()
 
 
 @then('we delete the room')
