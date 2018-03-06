a2c2fee0e7c5566fbb2d95fc26dfcec4f31c9f1c
DESKTOP-HRI2PAR\Евгений <e.anedchenko@room8studio.com>
2018-03-06 19:12:38 +0200

    Remove unnecessary code

--- pages/people_page.py
+++ pages/people_page.py
@@ -24,19 +24,6 @@ class PeoplePage(Page):
 
     def show_all_users(self):
         self.context.driver.find_element_by_id('show_all_users').click()
-        self.context.wait.until(EC.visibility_of_element_located((By.XPATH, '//ol[@class="aui-nav-pagination"]')))
-        users = self.context.driver.find_elements_by_xpath('//a[@class="name"]')
-        all_users = users.split()
-        print(all_users)
-
-    def we_see(self):
-        self.context.wait.until(EC.visibility_of_element_located((By.XPATH, '//ol[@class="aui-nav-pagination"]')))
-
-    def open_user_page(self):
-        self.context.driver.find_element_by_xpath('//a[contains(text(), "Test")]').click()
-
-    def label_page_head_on_user_page(self):
-        return self.context.driver.find_element_by_css_selector("div.aui-item > h2").text
 
     def open_user_page(self):
         xpath_for_search = '//a[contains(text(), "' + self.context.hipchat_full_name + '")]'
